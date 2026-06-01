b096779adef093b0bde14af9d951e58d70783e1d
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-06-01 13:40:13 +0400

    Add Booking class linking Guest and Room

--- GrandAzureHotel/Models/Booking.cs
+++ GrandAzureHotel/Models/Booking.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace GrandAzureHotel.Models
 {
-    internal class Booking
+    public class Booking
     {
+        //fields
+        private static int nextBookingID = 1001;
+        private int bookingID;
+        private Guest guest;
+        private Room room;
+
+
+        //properties
+        public int BookingID { get { return bookingID; } }
+        public Guest Guest { get { return guest; } }
+        public Room Room { get { return room; } }
+
+        //Constructor
+        public Booking(Guest guest, Room room)
+        {
+            bookingID = nextBookingID++;
+            this.guest = guest;
+            this.room = room;
+        }
+
+        //DisplayInfo Method
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"BookingID : {BookingID}");
+            Console.WriteLine($"Guest : {Guest.FullName}");
+            Console.WriteLine($"Room : {Room.RoomNumber}");
+            Console.WriteLine($"Type : {Room.RoomType}");
+
+        }
     }
 }
