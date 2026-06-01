43009d99d67bdd4156b7cdbc390007e640386e5d
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-06-01 12:53:12 +0400

    Add Guest class with encapsulation and static counter

--- GrandAzureHotel/Program.cs
+++ GrandAzureHotel/Program.cs
@@ -6,6 +6,7 @@ namespace GrandAzureHotel
     {
         static void Main(string[] args)
         {
+            //Test Guest Class
             Guest g1 = new Guest("Anoud", "999");
             Guest g2 = new Guest("Ahmed", "222");
             Console.WriteLine(Guest.GetTotalGuestsCreated());
--- GrandAzureHotel/obj/Debug/net10.0/GrandAzureHotel.AssemblyInfo.cs
+++ GrandAzureHotel/obj/Debug/net10.0/GrandAzureHotel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrandAzureHotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0455f28317fe828211d71c3e458785d36952bc09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffa1c2b3a4e49a54329e4a5420d20f0ce1df31c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrandAzureHotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrandAzureHotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
