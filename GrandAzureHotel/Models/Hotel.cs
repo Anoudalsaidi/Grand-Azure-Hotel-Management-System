86e3c4f41bb43eb06dfc051155948873e941e2d0
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-06-01 13:49:32 +0400

    Implement Hotel management logic (add, book, cancel, search)

---
+++ GrandAzureHotel/Models/Hotel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandAzureHotel.Models
+{
+    public class Hotel
+    {
+        //fields
+        private List<Guest> guests;
+        private List<Room> rooms;
+        private List<Booking> bookings;
+
+        //Auto Property for hotel name
+        public string HotelName { get; }
+
+        //Constructor
+        public Hotel(string name)
+        {
+            HotelName=name;
+            guests = new List<Guest>();
+            rooms = new List<Room>();
+            bookings = new List<Booking>();
+        }
+    }
+}
